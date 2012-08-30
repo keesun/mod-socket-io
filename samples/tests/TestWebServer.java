dabc89eac8e177c957c36afa8cc956d2c4627f0e
Keesun Baik <whiteship@epril.com>
2012-08-30 14:29:49 +0900

    added test web server

---
+++ samples/tests/TestWebServer.java
@@ -0,0 +1,47 @@
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.deploy.Verticle;
+
+/**
+ * @author Keesun Baik
+ */
+public class TestWebServer extends Verticle {
+
+	private static final String WEB_ROOT = "samples/web";
+
+	public TestWebServer(Vertx vertx) {
+		this.vertx = vertx;
+	}
+
+	@Override
+	public void start() throws Exception {
+		HttpServer server = vertx.createHttpServer();
+		server.requestHandler(new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				String filePath = WEB_ROOT;
+				String requestPath = request.path;
+				if(requestPath.equals("/")) {
+					filePath += "/index.html";
+				} else {
+					filePath += requestPath;
+				}
+
+				request.response.sendFile(filePath);
+			}
+		});
+
+		server.listen(8080);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Vertx vertx = Vertx.newVertx();
+		TestWebServer webServer = new TestWebServer(vertx);
+		webServer.start();
+		Thread.sleep(Long.MAX_VALUE);
+	}
+
+
+}
